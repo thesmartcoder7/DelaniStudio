1a046d88b27d9ab022cb17cbbda50d101d65b643
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-03-12 15:08:43 +0300

    style: add hover events to the portfolio section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/* - - - footer current year - - - */
+/* - - - footer copyright current year - - - */
 let timeline = new Date
 $(".current-year").text(timeline.getFullYear())
 
-/* - - - what we do onclick section - - - */
+/* - - - what we do onclick handler - - - */
 $(".default").click((e)=> {
     let siblingClassName = e.currentTarget.className.split(" ")[1]
     if(siblingClassName === "a"){
@@ -43,3 +43,43 @@ $(".description").click((e)=> {
     }
 })
 
+/* - - - portfolio hover handler - - - */
+let image = document.querySelectorAll(".item")
+let texBox = document.querySelectorAll(".text-box")
+let itemsArray = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+
+//mouseover function
+for(i=0; i<image.length; i++){
+    image[i].addEventListener("mouseover", (e) => {
+        let hoverTextClass = e.target.lastElementChild.className.split(" ")[1]
+        let backImage = e.target.firstElementChild
+        let targetDivClass = e.target.className
+        let targetDiv = document.querySelectorAll(`.${targetDivClass}`)
+        for(j=0; j<itemsArray.length; j++){
+            if(hoverTextClass === itemsArray[j] ){
+                $(`.${hoverTextClass}`).css({
+                    'display': 'flex'
+                })
+                backImage.style.filter = 'brightness(50%)'
+            }
+        }   
+    })
+}
+
+//mouseout function
+for(i=0; i<image.length; i++){
+    image[i].addEventListener("mouseout", (e) => {
+        let hoverTextClass = e.target.lastElementChild.className.split(" ")[1]
+        let targetDivClass = e.target.className
+        let backImage = e.target.firstElementChild
+        let targetDiv = document.querySelectorAll(`.${targetDivClass}`)
+        for(j=0; j<itemsArray.length; j++){
+            if(hoverTextClass === itemsArray[j] ){
+                $(`.${hoverTextClass}`).css({
+                    'display': 'none'
+                })
+                backImage.style.filter = 'brightness(100%)'
+            }
+        }   
+    })
+}
